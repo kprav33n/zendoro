e3b819f62ac8e773deebaae3ac6ba8daecc8abf2
Praveen Kumar <praveen+git@kumar.in>
2017-11-24 17:19:56 -0800

    Implement task, and cancel support.
    Also setup eslint, and cleanup issues reported by it.

--- main.js
+++ main.js
@@ -1,17 +1,105 @@
-const { app, Tray, Menu, BrowserWindow } = require('electron');
+const {
+  Menu,
+  Tray,
+  app,
+  globalShortcut,
+} = require('electron');
 const path = require('path');
 
-let tray = null;
+function registerGlobalShortcuts(tray) {
+  // FIXME: This doesn't play well with alternate keybard layouts.
+  globalShortcut.register('CmdOrCtrl+Alt+R', () => { tray.popUpContextMenu(); });
+}
 
-app.on('ready', function () {
-      tray = new Tray(path.join(__dirname, 'assets/images/TrayIconTemplate.png'));
-      const contextMenu = Menu.buildFromTemplate([
-            {
-                  label: 'Quit',
-                  accelerator: 'Command+Q',
-                  selector: 'terminate:',
-            }
-      ]);
-      tray.setToolTip('Zendoro');
-      tray.setContextMenu(contextMenu);
+// FIXME: Verify if this function is written in JS idiomatic way.
+function timeToString(ms) {
+  let t = Math.trunc(ms / 1000);
+  const seconds = t % 60;
+  t = Math.trunc(t / 60);
+  const minutes = t % 60;
+  t = Math.trunc(t / 60);
+  const hours = t % 24;
+  t = Math.trunc(t / 24);
+  const days = t;
+  let daysPrefix = '';
+  if (days > 1) {
+    daysPrefix = `${days} days, `;
+  } else if (days > 0) {
+    daysPrefix = `${days} day, `;
+  }
+  let hoursPrefix = '';
+  if (hours > 0) {
+    hoursPrefix = `${hours.toString().padStart(2, '0')}:`;
+  }
+  return `${daysPrefix}${hoursPrefix}${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+}
+
+function updateCounter(tray, end) {
+  const remaining = end - Date.now();
+  tray.setTitle(timeToString(remaining));
+}
+
+function clearCounter(tray) {
+  tray.setTitle('');
+}
+
+/* eslint-disable no-param-reassign */
+function idleState(contextMenu) {
+  contextMenu.items[0].enabled = true;
+  contextMenu.items[1].enabled = false;
+}
+
+function timerState(contextMenu) {
+  contextMenu.items[0].enabled = false;
+  contextMenu.items[1].enabled = true;
+}
+/* eslint-enable no-param-reassign */
+
+
+function startTask(tray, contextMenu, interval) {
+  timerState(contextMenu);
+  if (interval != null) {
+    return interval;
+  }
+  const taskLength = (25 * 60 * 1000);
+  const end = new Date(Date.now() + taskLength).getTime();
+  updateCounter(tray, end);
+  return setInterval(() => { updateCounter(tray, end); }, 500);
+}
+
+function cancelTimer(tray, contextMenu, interval) {
+  idleState(contextMenu);
+  clearInterval(interval);
+  clearCounter(tray);
+}
+
+app.on('ready', () => {
+  const tray = new Tray(path.join(__dirname, 'assets/images/TrayIconTemplate.png'));
+  let interval = null;
+  const contextMenu = Menu.buildFromTemplate([
+    {
+      label: 'Start a task',
+      accelerator: 'T',
+      click() { interval = startTask(tray, contextMenu, interval); },
+    },
+    {
+      label: 'Cancel',
+      accelerator: 'C',
+      click() { cancelTimer(tray, contextMenu, interval); interval = null; },
+    },
+    {
+      label: 'Quit',
+      accelerator: 'Q',
+      selector: 'terminate:',
+    },
+  ]);
+  tray.setToolTip('Zendoro');
+  tray.setContextMenu(contextMenu);
+  idleState(contextMenu);
+
+  registerGlobalShortcuts(tray);
+});
+
+app.on('will-quit', () => {
+  globalShortcut.unregisterAll();
 });
